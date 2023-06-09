9fe9b235af90f9c0b60022641825425a26b92ab8
Esubef <136054259+Esubef@users.noreply.github.com>
2023-06-09 10:24:11 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+  <title>Document </title>
+    </head>
+    <body>
+        <h1>Document</h1>
+        <br>
+        <img src="a.jpeg" alt="" width="100%">
+        <img src="b.jpeg" alt="" width="100%">
+        <img src="c.jpeg" alt="" width="100%">
+        
+    </body>
+</html>
